0910cbb075e6c6adde6567bccc69dab59cb2a2b9
Deyvi Condori <deivcondor@gmail.com>
2023-10-06 14:35:36 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio 7 - Prbl 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Secuencias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el valor de n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            double sumaTotal = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double resultado = (16.0 - i) / (3.0 * i);
+                Console.WriteLine($"resultado : {resultado}");
+                sumaTotal += resultado;
+            }
+
+            Console.WriteLine($"{sumaTotal}");
+        }
+    }
+}
